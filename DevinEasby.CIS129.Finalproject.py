2c493954c30304afb9ab39fc175906780fad0086
deasby <deasby@mail.pima.edu>
2024-12-17 09:29:40 -0700

    Create DevinEasby.CIS129.Finalproject.py
    https://docs.google.com/presentation/d/1Z7zSeVjwqQKD0dazbddTkt5szSuKTcZPqvJSUfTUozk/edit#slide=id.g31faac60e5a_0_35 
    https://docs.google.com/document/d/1VgQ6GAHCV3J4HmnI5LpVYy3Ujiy1SawiW8sP2raEWqA/edit?tab=t.0

---
+++ DevinEasby.CIS129.Finalproject.py
@@ -0,0 +1,20 @@
+# Devin Easby
+# CIS 129
+# Psuedocode for final project
+1. Initialize an empty list of tasks. 
+2. Create a function to add a new task: 
+Ask for the task name, description, and priority.
+Create a new task object with attributes: name, description, priority and completion status.
+Add the task to the list.
+3. Create a function to edit an existing task: 
+Ask for the task to edit. (Search using task name)
+Modify the task attributes. (Name, Description, Priority)
+4. Create a function to delete a task: 
+Ask for the task to delete. (Search using task name)
+Remove the task from the task list.
+5. Create a function to mark the task as complete: 
+Ask for the task to mark as complete. (Search using task name)
+Change the tasks status to “Completed”. 
+6. Create a function to filter tasks: 
+Allow filtering by priority (High, Medium, Low) 
+Allow filtering by status (Completed or Pending) 
